ad96ebfe0062a4dc1cf9a36c8976fff33bfd3e9a
Viet <0209.nhuviet@gmail.com>
2025-01-06 11:25:22 +0700

    update problem 4

--- src/problem4/sum.tsx
+++ src/problem4/sum.tsx
@@ -1,4 +1,4 @@
-function sum_to_n_a(n: number): number {
+function sum_to_n_a2(n: number): number {
     let sum = 0;
     for (let i = 1; i <= n; i++) {
         sum += i;
@@ -6,11 +6,11 @@ function sum_to_n_a(n: number): number {
     return sum;
 }
 
-function sum_to_n_b(n: number): number {
+function sum_to_n_b2(n: number): number {
     return (n * (n + 1)) / 2;
 }
 
-function sum_to_n_c(n: number): number {
+function sum_to_n_c2(n: number): number {
     if (n === 1) return 1;
     return n + sum_to_n_c(n - 1);
 }
